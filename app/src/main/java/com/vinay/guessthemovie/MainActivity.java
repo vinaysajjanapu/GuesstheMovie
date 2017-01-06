bc0932413e38d3cf274b4dca0c011a2d67354202
vinaysajjanapu <vinaysajjanapu@gmail.com>
2017-01-06 15:27:58 +0530

    holder text size

--- app/src/main/java/com/vinay/guessthemovie/MainActivity.java
+++ app/src/main/java/com/vinay/guessthemovie/MainActivity.java
@@ -13,7 +13,8 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    String[] db = {"dhruva","roy","ram"};
+    String[] db = {"dhruva","roy","ram","ekkadiki pothavu chinnavada","saptagiri express","vangaveeti",
+    "dangal","khaidi 150","gautamiputra","satamanam bhavathi","raaes","kaabil"};
     String moviename;
     LinearLayout ll_holder;
     LinearLayout[] holder_row;
@@ -45,7 +46,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void Initialize() {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         getSupportActionBar().hide();
-        num_col=6;
+        num_col=8;
         five=5;
         finish=0;
 
@@ -66,8 +67,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         holder_row = new LinearLayout[(moviename.length()/num_col)+1];
         iv_holder = new Button[moviename.length()];
         lp1 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        int width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50, getResources().getDisplayMetrics());
-        lp2 = new LinearLayout.LayoutParams(width, ViewGroup.LayoutParams.WRAP_CONTENT);
+        //int width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50, getResources().getDisplayMetrics());
+        lp2 = new LinearLayout.LayoutParams(screenWidth/(num_col), ViewGroup.LayoutParams.WRAP_CONTENT);
 
         for(int i=0; i<=(moviename.length()/num_col);i++){
             holder_row[i] = new LinearLayout(this);
@@ -77,6 +78,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             for (int j=0; j<r;j++){
                 iv_holder[i*num_col+j]=new Button(this);
                 iv_holder[i*num_col+j].setLayoutParams(lp2);
+                iv_holder[i*num_col+j].setTextSize(screenWidth/(2*num_col));
                 Character c=moviename.charAt(i*num_col+j);
                 if(!(c.toString().equals(" "))){
                     iv_holder[i*num_col+j].setText("_");
@@ -92,7 +94,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         String[][] str = new String[][]{
                 {"0","1","2","3","4","5","6","7","8","9"},
                 {"Q","W","E","R","T","Y","U","I","O","P"},
-                {"A","S","D","F","G","H","J","K","L","-"},
+                {"A","S","D","F","G","H","J","K","L",""},
                 {"","Z","X","C","V","B","N","M","",""},
         };
         keyboard = (LinearLayout)findViewById(R.id.keyboard);
@@ -124,36 +126,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button key = (Button)view;
         Boolean b=false;
-        for (int v=0;v<moviename.length();v++){
+        for (int v=0;v<moviename.length();v++) {
 
-            if(!key.getText().toString().equals(""))
-            if(Character.toString(moviename.charAt(v)).matches(key.getText().toString().toLowerCase())){
+            if (!key.getText().toString().equals("")) {
+                if (Character.toString(moviename.charAt(v)).matches(key.getText().toString().toLowerCase())) {
 
-                iv_holder[v].setText(key.getText().toString());
-                b=true;
-                finish++;
-                //Toast.makeText(getApplicationContext(),finish+"",Toast.LENGTH_SHORT).show();
-            };
-        }
-
-        if(!b) {
-            five--;
-            hint.setText("wrong attempt "+ (5-five));
-        }
+                    iv_holder[v].setText(key.getText().toString());
+                    b = true;
+                    finish++;
+                    //Toast.makeText(getApplicationContext(),finish+"",Toast.LENGTH_SHORT).show();
+                }
 
 
-        if(finish==moviename.length() || (five==0)){
-            if (five==0){
-                hint.setText("All lifes over");
+                if (!b) {
+                    five--;
+                    hint.setText("wrong attempt " + (5 - five));
+                }
 
 
-            }else {
-                hint.setText("Congratulations");
+                if (finish == moviename.length() || (five == 0)) {
+                    if (five == 0) {
+                        hint.setText("All lifes over");
+
+
+                    } else {
+                        hint.setText("Congratulations");
+                    }
+
+                    //this.recreate();
+                }
             }
-
-            //this.recreate();
         }
-
     }
 
 }
